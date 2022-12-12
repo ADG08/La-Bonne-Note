6c06edfbe83633abc5a85f373f9bb70648c53e38
SoaZy <eric.ye@etu.u-paris.fr>
2022-12-12 17:49:41 +0100

    modif

--- Register/register.php
+++ Register/register.php
@@ -1,4 +1,26 @@
 <?php
+session_start();
+
+    include("login.php");
+
+    if($_SERVER['REQUEST_METHOD']=="POST"){
+        $prenom = $_POST['prenom'];
+        $mdp= $_POST['mdp'];
+
+        if(!empty($prenom) && !empty($mdp)){
+            //sauvegarde dans la base de données
+            $query = "insert into utilisateur (Prenom, mdp, email) values ('$Prenom', '$mdp', '$email')";
+
+            mysqli_query($con,$query);
+
+            header("Location: login.php");
+            die;
+        }else{
+            echo "Entrez des informations valide"
+        }
+    }
+
+
 
 
 ?>
