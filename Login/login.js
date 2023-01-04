eb33bacdc5a4f7d01be9cab5279e68ca3739c823
babyIsk <115020880+babyIsk@users.noreply.github.com>
2023-01-05 00:23:07 +0100

    eye

--- Login/login.js
+++ Login/login.js
@@ -1,23 +1,21 @@
-const container = document.querySelector(".container"),
-    showHide = document.querySelectorAll(".showHide"),
-    password = document.querySelectorAll(".password");
+const   showHide = document.querySelectorAll(".showHide"),
+        password = document.querySelectorAll(".password");
+        showHide.forEach(eyeIcon =>{
+            eyeIcon.addEventListener("click", ()=>{
+                password.forEach(password =>{
+                    if(password.type=="password"){
+                        password.type ="text";
 
-    showHide.forEach(eyeIcon =>{
-        eyeIcon.addEventListener("click", ()=>{
-            password.forEach(password =>{
-                if(password.type=="password"){
-                    password.type ="text";
+                        showHide.forEach(icon =>{
+                            icon.classList.replace("fa-eye-slash","fa-eye")
+                        })
+                    }else{
+                        password.type ="password";
 
-                    showHide.forEach(icon =>{
-                        icon.classList.replace("fa-eye-slash","fa-eye")
-                    })
-                }else{
-                    password.type ="password";
-
-                    showHide.forEach(icon =>{
-                        icon.classList.replace("fa-eye","fa-eye-slash")
-                    })
-                }
+                        showHide.forEach(icon =>{
+                            icon.classList.replace("fa-eye","fa-eye-slash")
+                        })
+                    }
+                })
             })
         })
-    })
--- Login/login.tpl
+++ Login/login.tpl
@@ -9,7 +9,6 @@
   <title>Login - La Bonne Note</title>
   <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
   <script src="https://kit.fontawesome.com/ac2f7dafa4.js" crossorigin="anonymous"></script>
-  <script src="login.js"></script>
 </head>
 
 <body>
@@ -35,9 +34,11 @@
         <div class="text">
           <small>Pas encore inscrit(e) ? <a href="../Register/register.html">S'inscrire</a> </small>
         </div>
+	<?php require("login.php");   ?>
       </form>
     </div>
   </div>
+<script src="login.js"></script>
 </body>
 
 </html>
