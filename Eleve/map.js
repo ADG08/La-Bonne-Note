af2b2a7e15dd33cbdde5d9aab89b717b03303f63
ADG08 <49250104+ADG08@users.noreply.github.com>
2023-01-05 16:10:26 +0100

    pouf

--- Eleve/map.js
+++ Eleve/map.js
@@ -9,6 +9,26 @@ $(document).ready(function () {
         [48.84197804895268, 2.267719848410252],
     ];
 
+    var favoriIcon = L.icon({
+        iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-gold.png',
+        shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowSize: [41, 41]
+    });
+
+    var defaultIcon = L.icon({
+        iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+        shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowSize: [41, 41]
+    });
+    L.mar
+
+
     var map = L.map("maDiv").setView([48.856614, 2.3522219], 12);
 
     L.tileLayer(
@@ -33,21 +53,85 @@ $(document).ready(function () {
         console.log(arrondissements.features[i]);
     }
 
+<<<<<<< Updated upstream
+=======
+    function focus(event) {
+
+        for (let y = 0; y < arrondissements.features.length; y++) {
+
+            x[y].setStyle({ fillColor: '#68D89B', weight: 1, color: '#2C6E49', opacity: 1 })
+
+
+            x[y].setStyle({ opacity: 0.4 })
+
+            if (y != event.data.param) {
+
+                x[event.data.param].setStyle({ opacity: 1 })
+            }
+
+        }
+        x[event.data.param].setStyle({ fillColor: '#BCFFDB', weight: 2, color: '#2C6E49', opacity: 1 })
+
+        $.ajax({
+            type: "POST",
+            url: "recupProf.php",
+            data: {
+                "arr": index[event.data.param]
+            },
+            success: function (res) {
+
+                JSON.parse(res).forEach(element => {
+                    console.log(element)
+                    var x = L.marker([element.Latitude, element.Longitude], { icon: defaultIcon })
+                        .bindPopup(element.Nom + " " + element.Prénom)
+                        .addTo(map)
+                        .on("click", clickZoom)
+                });
+            },
+            error: function (err) {
+                console.error(err);
+            },
+        });
+    }
+
+
+    $.ajax({
+        type: "POST",
+        url: "recupProfFav.php",
+        success: function (res) {
+
+            console.log(res)
+
+            JSON.parse(res).forEach(element => {
+                console.log(element)
+                var x = L.marker([element.Latitude, element.Longitude], { icon: favoriIcon })
+                    .bindPopup("<b>" + element.Nom + "</b> " + element.Prénom)
+                    .addTo(map)
+                    .on("click", clickZoom)
+            });
+        },
+        error: function (err) {
+            console.error(err);
+        },
+    });
+
+
+>>>>>>> Stashed changes
     //marker
 
-    var littleton = L.marker([48.841503, 2.247047])
+    var littleton = L.marker([48.841503, 2.247047], { icon: favoriIcon })
         .bindPopup("This is Littleton, CO.")
         .addTo(map)
         .on("click", clickZoom),
-        denver = L.marker([48.829978, 2.280436])
+        denver = L.marker([48.829978, 2.280436], { icon: defaultIcon })
             .bindPopup("This is Denver, CO.")
             .addTo(map)
             .on("click", clickZoom),
-        aurora = L.marker([48.881425, 2.277603])
+        aurora = L.marker([48.881425, 2.277603], { icon: defaultIcon })
             .bindPopup("This is Aurora, CO.")
             .addTo(map)
             .on("click", clickZoom),
-        golden = L.marker([48.892938, 2.411499])
+        golden = L.marker([48.892938, 2.411499], { icon: favoriIcon })
             .bindPopup("This is Golden, CO.")
             .addTo(map)
             .on("click", clickZoom);
