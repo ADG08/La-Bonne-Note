1b40361cc238a8a8c3ea3552513ebfc55b491558
babyIsk <115020880+babyIsk@users.noreply.github.com>
2023-01-06 06:41:13 +0100

    boutton déco

--- Prof/prof.php
+++ Prof/prof.php
@@ -1,9 +1,15 @@
 <?php
 session_start();
-if ($_POST["deco"]) {
+
+if(isset($_POST["deco"])) {
     session_destroy();
+    header("Location:http://localhost:59000/la-bonne-note/Login/login.php");
 }
+
+if(isset($_SESSION['profil'])) {
 require("prof.tpl");
+}else {
 
-
+    header("Location:http://localhost:59000/la-bonne-note/Login/login.php");
+}
 ?>
